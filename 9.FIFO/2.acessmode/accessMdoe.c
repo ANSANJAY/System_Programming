661a974dc1243ef2a57edb0420bfa5e6b58b5c46
ANSANJAY <97059168+ANSANJAY@users.noreply.github.com>
2022-12-27 19:26:26 -0500

    Add files via upload

---
+++ 9.FIFO/2.acessmode/accessMdoe.c
@@ -0,0 +1,29 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<limits.h>
+
+#define FIFO_NAME "./my_fifo"
+
+int main()
+{
+int ret;
+if (access(FIFO_NAME, F_OK)==-1)
+{
+	ret = mkfifo(FIFO_NAME,0666);
+	if( ret == -1)
+	{
+		fprintf(stderr,"FIFO not created %s\n",FIFO_NAME);
+		exit(EXIT_FAILURE);
+	}
+	printf("process %d created FIFO\n",getpid());
+	exit(EXIT_SUCCESS);
+
+}
+printf("FIFO: %s already created\n",FIFO_NAME);
+exit(EXIT_SUCCESS);
+}
